b5be297aa8308fc57368af3ba9d4feab0fddbde1
Diego Lopez Gutierrez <diego_lopezg@hotmail.com>
2023-09-11 20:49:37 -0500

    Add build directory

---
+++ build/CMakeFiles/mesonProduction.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mesonProduction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mesonProduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
