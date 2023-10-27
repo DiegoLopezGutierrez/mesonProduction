d999c073aa4edfe39dcdca33533a0c3491807759
Diego Lopez Gutierrez <diego_lopezg@hotmail.com>
2023-10-26 21:45:18 -0500

    Stop tracking build/ directory

--- build/CMakeFiles/mesonProduction.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mesonProduction"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mesonProduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/mesonProduction.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for mesonProduction.
-# This may be replaced when dependencies are built.
